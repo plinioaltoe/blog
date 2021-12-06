7caa533cfdfe55cbe981492f8bc7c64ec1011b36
Plinio Altoe Costa Vieira <plinio.altoe@edirectinsure.com>
2021-12-06 23:18:51 +0000

    get static props

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,9 +1,14 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 
+
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { RichText } from 'prismic-dom';
 
 interface Post {
   first_publication_date: string | null;
@@ -26,20 +31,52 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post() {
- return (<div>hey joe</div>)
+export default function Post({ post }) {
+  console.log((post))
+  return (<div>{post.data.content.heading}</div>)
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+};
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('posts', String(slug), {});
+  console.log((response?.data?.title))
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+  const post = {
+    slug,
+    first_publication_date: format(
+      new Date(response.first_publication_date),
+      "dd MMM yyyy",
+      {
+        locale: ptBR,
+      }
+    ),
+    data: {
+      title: (response?.data?.title),
+      banner: {
+        url: response.data?.banner?.url
+      },
+      author: (response.data?.author),
+      content: response.data?.content.map(c => ({
+        heading: c?.heading,
+        body: { text: RichText.asHtml(c?.body) },
+      }))
+    }
+  }
 
-//   // TODO
-// };
+  console.log(post)
+
+  return {
+    props: {
+      post
+    }
+  }
+};
+
